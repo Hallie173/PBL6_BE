5a1b981fc751ff370199501eda549fa9500aca70
g15 5511 <phuonghaleduy@gmail.com>
2025-11-07 09:26:57 +0700

    Tạo cấu trúc thư mục hoàn chỉnh

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOST_NAME;
+
+//config template engine
+app.set("views", path.join(__dirname, "./views/"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello World! I am Hallie!");
+});
+
+app.get("/halliepage", (req, res) => {
+  //res.send("Hello World! I am Hallie!");
+  res.render("sample.ejs");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Example app listening on port ${port}`);
+});
